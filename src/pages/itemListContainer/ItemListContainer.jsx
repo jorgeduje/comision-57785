a79d3c9d077ec885d4c71d6b94ccb8255be92a72
jorgeduje <jorgeduje4@gmail.com>
2024-06-13 20:55:27 -0300

    se trabaja con promesas y map

--- src/pages/itemListContainer/ItemListContainer.jsx
+++ src/pages/itemListContainer/ItemListContainer.jsx
@@ -1,7 +1,34 @@
 import ItemList from "./ItemList";
+import { products } from "../../products";
+import { useState, useEffect } from "react";
 
 const ItemListContainer = () => {
-  return <ItemList />;
+  // una peticion que me traiga los productos del backend
+
+  const [items, setItems] = useState([]);
+  const [error, setError] = useState({});
+
+  useEffect(() => {
+    const getProducts = new Promise((resolve, reject) => {
+      let x = true;
+      if (x) {
+        resolve(products);
+      } else {
+        reject({ message: "error", codigo: "404" });
+      }
+    });
+
+    // manejar la promesa
+    getProducts
+      .then((res) => {
+        setItems(res);
+      })
+      .catch((error) => {
+        setError(error);
+      });
+  }, []);
+
+  return <ItemList items={items} />;
 };
 
 export default ItemListContainer;
